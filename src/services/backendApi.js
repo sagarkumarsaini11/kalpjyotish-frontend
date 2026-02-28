eab004395d5c9c14b28ed57d41efdc99c49174a2
Viraj Goldy <abhisekgoldy14@gmail.com>
2026-02-28 22:07:14 +0530

    checking with local url

--- src/services/backendApi.js
+++ src/services/backendApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://backend.kalpjyotish.com";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const backendApi = createApi({
   reducerPath: "backendApi",
